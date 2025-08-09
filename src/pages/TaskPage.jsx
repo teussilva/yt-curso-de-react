de3ccd6a819e97c0091e2c27da6dcc4f7e047955
Matheus Henrique <mh8093828@gmai.com>
2025-08-09 16:54:54 -0300

    correcao da tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -10,9 +10,7 @@ function TaskPage() {
 
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] space-y-4 mx-auto">
         <div className="flex justify-center relative ">
           <button
             onClick={() => navigate(-1)}
